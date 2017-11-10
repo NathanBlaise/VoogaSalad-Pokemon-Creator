4437127b7547dc83d7704df98a04d4dd07df41b6
Dan Sun <ds318@duke.edu>
2017-11-10 00:38:29 -0500

    added player api and usecase

---
+++ src/util/Coordinates.java
@@ -0,0 +1,12 @@
+package util;
+
+import javafx.geometry.Point2D;
+
+public class Coordinates {
+
+    private Point2D coordinates;
+    
+    public Coordinates(double x, double y) {
+	coordinates = new Point2D(x,y);
+    }
+}
