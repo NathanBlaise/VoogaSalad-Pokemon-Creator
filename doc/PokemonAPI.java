acd82371321fb58a7100a2a31ce9d21c4f8851b7
Yiqin Zhou <yz259@duke.edu>
2017-11-09 21:05:10 -0500

    Added pokemon api

---
+++ doc/PokemonAPI.java
@@ -0,0 +1,11 @@
+
+public class PokemonAPI {
+	public void pokemonMove(double x, double y);
+	public double getPokemonXCor();
+	public double getPokemonYCor();
+	public double getHealth();
+    public void pokemonRefill(double boost);
+    
+	
+
+}
