c87b8496977b0d5b40a7c641e02b2a06e9c26cc1
cy122 <cy122@duke.edu>
2017-11-12 00:04:36 -0500

    save to work tomorrow

---
+++ src/authoring/eventManage/EventEditor.java
@@ -0,0 +1,40 @@
+package authoring.eventManage;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.HashMap;
+
+import data.PropertyReader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+/**
+ * 
+ * @author cy122
+ * 
+ * This provides a UI to let user edit the event
+ *
+ */
+
+public class EventEditor {
+	private Stage stage =  new Stage();
+	private EventRegister eventRegister = new EventRegister();
+	private BorderPane root = new BorderPane();
+	private EventImage eventImage;
+	//TODO: load the actual data into this
+	private EventInstructions eventInstructions = new EventInstructions(new HashMap<String,String>());
+	private Scene scene = new Scene(root);
+	
+	public EventEditor(EventImage eventImage){
+		this.eventImage = eventImage;
+		this.eventImage.addFileChooser(root);
+		//TODO: add the whole components to event editor
+		root.setLeft(eventImage);
+		root.setCenter(eventInstructions);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+}
---
+++ src/authoring/eventManage/EventImage.java
@@ -0,0 +1,70 @@
+package authoring.eventManage;
+
+import java.io.File;
+
+import javafx.event.EventHandler;
+import javafx.scene.Cursor;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+
+public class EventImage extends BorderPane{
+	private String imagePath;
+	private ImageView imageHolder;
+	
+	/**
+	 * 
+	 * @param imagePath - must be a absolute path
+	 * @param node
+	 */
+	public EventImage(String imagePath){
+		super();
+		Image image = new Image(new File(imagePath).toURI().toString());
+		imageHolder = new ImageView(image);
+		setCenter(imageHolder);
+		setTop(new Label("↓Double Click To Change Image"));
+		String style_outter="-fx-border-color: black;-fx-border-style: solid;-fx-border-width: 5;";
+		imageHolder.setStyle(style_outter);
+		imageHolder.setOnMouseEntered(e->{setCursor(Cursor.HAND);});
+		imageHolder.setOnMouseExited(e->{setCursor(Cursor.DEFAULT);});
+		this.imagePath = new String(imagePath);
+	}
+	
+	public void addFileChooser(Node node){
+		this.setOnMouseClicked(new EventHandler<MouseEvent>() {
+		    @Override
+		    public void handle(MouseEvent click) {
+		        if (click.getClickCount()==2) {
+		           openFileChoose(node.getScene().getWindow());
+		        }
+		    }
+		});
+	}
+	
+	public void openFileChoose(Window window){
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open Resource File");
+		fileChooser.setInitialDirectory(new File("images"));
+		File file = fileChooser.showOpenDialog(window);
+		if(file!=null){
+			Image newImage = new Image(file.toURI().toString());
+			if(newImage!=null){
+				imageHolder.setImage(newImage);
+				imagePath = file.getAbsolutePath();
+			}
+		}
+	}
+	
+	public String getImagePath(){
+		return new String(imagePath);
+	}
+	
+	public Image getImage(){
+		return imageHolder.getImage();
+	}
+}
