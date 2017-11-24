46b11ecb10ab02592e994db91615e3ec4c75ac99
Dan Sun <ds318@duke.edu>
2017-11-24 16:09:34 -0600

    fixed bugs

---
+++ src/tests/pokemonSpecieParser/PokemonSpecieParserTest.java
@@ -0,0 +1,28 @@
+package tests.pokemonSpecieParser;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import data.model.PokemonSpecie;
+import util.FilePathConverter;
+import util.PokemonSpecieFileParser;
+
+public class PokemonSpecieParserTest {
+
+    private String testSpecieFilePath = "src/resources/defaultPokemonSpecies/Pikachu.xml";
+    @Test
+    public void test() throws ParserConfigurationException, SAXException, IOException {
+	//create new parser for testing
+	PokemonSpecieFileParser specieParser = new PokemonSpecieFileParser();
+	PokemonSpecie specie = specieParser.parseFile(
+		FilePathConverter.getAbsolutePath(testSpecieFilePath));
+	assertTrue(true);
+    }
+
+}
