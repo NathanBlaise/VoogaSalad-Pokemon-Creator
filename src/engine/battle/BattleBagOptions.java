da37d46d6339364b8d3b28637d3f559a57ba4236
Nathan Lewis <nathanblaiselewis@gmail.com>
2017-11-26 18:46:35 -0500

    Fight button functionality completed, other buttons to do and flow of battle to be done

---
+++ src/engine/battle/BattleBagOptions.java
@@ -0,0 +1,18 @@
+package engine.battle;
+
+import data.player.Player;
+
+/**
+ * Class to create the buttons and show item options for when bag button is chosen
+ * @author nathanlewis
+ *
+ */
+public class BattleBagOptions {
+	
+	private Player mainPlayer;
+	
+	public BattleBagOptions(Player player) {
+		mainPlayer = player;
+	}
+	
+}
