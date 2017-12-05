0b8aee77053158f1e901d52b8eaa98cb64479bf1
Yiqin Zhou <yz259@duke.edu>
2017-12-04 22:58:51 -0500

    Random stuff

--- src/engine/battle/BattleScene.java
+++ src/engine/battle/BattleScene.java
@@ -181,7 +181,7 @@ public class BattleScene extends ScreenDisplay{
 			ebfo=new EnemyBattleFightOptions(activePokemon,enemyPokemon,this);
 	
 			bfo.setUpScene();
-			createEndButton();
+			
 			
 			
 			
@@ -192,20 +192,7 @@ public class BattleScene extends ScreenDisplay{
 		
 	}
 	
-	private void createEndButton() {
-		Button end=new Button("End");
-		end.setTranslateX(100);
-		end.setTranslateY(200);
-		this.rootAdd(end);
-		end.setOnAction((event) -> {
-			this.rootRemove(bfo.getHBox());
-			this.rootRemove(bfo.getText());
-			this.rootRemove(ebfo.getText());	
-			this.rootRemove(ebfo.getHBox());
-			//this.rootAdd(hbox);
-		});
-		
-	}
+	
 	
 	public BattleFightOptions getMyBattleScene() {
 		return bfo;
