4ce54e4b873d0cac144a10b70316d36e7f0f85c8
Nathan Lewis <nathanblaiselewis@gmail.com>
2017-12-05 01:04:50 -0500

    Shop scene added where you can select and purchase items

--- src/data/player/Player.java
+++ src/data/player/Player.java
@@ -15,25 +15,15 @@ import engine.movement.Direction;
  */
 public class Player implements Serializable{
 	private static final long serialVersionUID = 556462866183029469L;
-
-	
-	private final static int speed = 3;
-	//only holds these attributes for now
 	private int posX, posY;
-	public transient int rightspeed, leftspeed, downspeed, upspeed;
-	private Pokemon[] pokemons; //the pokemons for battle, no pokemon in the warehouse for now
-	private ArrayList<Item> items; //Items held by the player
+	private Pokemon[] pokemons;
+	private ArrayList<Item> items;
 	public transient Direction direction;
 	//private Map<Item,Integer> Bag;  // a map from the item to the number of item
 	
 	public Player(){
 		posX=50;
 		posY=50;
-		rightspeed = speed;
-		leftspeed = speed;
-		upspeed = speed;
-		downspeed = speed;
-//		direction = DOWN;
 		pokemons = new Pokemon[6];
 	}
 	
@@ -52,69 +42,6 @@ public class Player implements Serializable{
 	public void setPosY(int posY) {
 		this.posY = posY;
 	}
-	
-//	/*
-//	 * Processes input using the input class
-//	 */
-//	public void processInput(Input input) {
-//		if(canMove) {
-//			if( input.isMoveUp()) {
-//				moveUp();
-//	        } else if( input.isMoveDown()) {
-//	        		moveDown();
-//	        } 
-//			
-//			if( input.isMoveLeft()) {
-//				moveLeft();
-//	        } else if( input.isMoveRight()) {
-//	        		moveRight();
-//	        } 
-//		}
-//	}
-	
-	// add bound over here
-	
-	public void moveUp() {
-		if (posY <= 45) {
-			
-		}
-		
-		else {
-		posY = posY - upspeed;
-		}
-	}
-	
-	public void moveDown() {
-		if (posY >= 480) {
-			
-		}
-		
-		else {
-		posY = posY + downspeed;
-		}
-	}
-	
-	public void moveLeft() {
-		
-		if (posX <= 40) {
-			
-		}
-		
-		else {
-		posX = posX - leftspeed;
-		}
-	}
-	
-	public void moveRight() {
-		if (posX >= 720  ) {
-			
-		}
-		
-		else {
-		posX = posX + rightspeed;
-		}
-	}
-
 
 	public Pokemon[] getPokemons() {
 		return pokemons;
@@ -126,6 +53,10 @@ public class Player implements Serializable{
 
 	public void setItems(ArrayList<Item> items) {
 		this.items = items;
+	}
+	
+	public void addItem(Item item) {
+		items.add(item);
 	}
 
 	public void setPokemons(Pokemon[] pokemons) {
--- src/engine/movement/Direction.java
+++ src/engine/movement/Direction.java
@@ -10,10 +10,10 @@ import java.util.ArrayList;
  */
 public class Direction {
 	public enum pictures{
-		UP(emerald_up_rest, emerald_up_1, emerald_up_2),
-		DOWN(emerald_down_rest, emerald_down_1, emerald_down_2),
-		LEFT(emerald_left_rest, emerald_left_1, emerald_left_2),
-		RIGHT(emerald_right_rest, emerald_right_1, emerald_right_2);
+		UP(sprite_up_rest, sprite_up_1, sprite_up_2),
+		DOWN(sprite_down_rest, sprite_down_1, sprite_down_2),
+		LEFT(sprite_left_rest, sprite_left_1, sprite_left_2),
+		RIGHT(sprite_right_rest, sprite_right_1, sprite_right_2);
 		
 		private ArrayList<Image> images = new ArrayList<Image>();
 		
@@ -28,40 +28,19 @@ public class Direction {
 		}
 	}
 
-	static {
-		emerald_down_rest = new Image("file:images/emerald_down_rest.png");
-		emerald_down_1 = new Image("file:images/emerald_down_1.png");
-		emerald_down_2 = new Image("file:images/emerald_down_2.png");
-		emerald_left_rest = new Image("file:images/emerald_left_rest.png");
-		emerald_left_1 = new Image("file:images/emerald_left_1.png");
-		emerald_left_2 = new Image("file:images/emerald_left_2.png");
-		emerald_right_rest = new Image("file:images/emerald_right_rest.png");
-		emerald_right_1 = new Image("file:images/emerald_right_1.png");
-		emerald_right_2 = new Image("file:images/emerald_right_2.png");
-		emerald_up_rest = new Image("file:images/emerald_up_rest.png");
-		emerald_up_1 = new Image("file:images/emerald_up_1.png");
-		emerald_up_2 = new Image("file:images/emerald_up_2.png");
-	}
-	
-	
-	/*
-	 * NEEDS REFACTORING!
-	 */
-	public static final transient Image emerald_down_rest; //Don't need all of these
-	public static final transient Image emerald_down_1;
-	public static final transient Image emerald_down_2;
-	public static final transient Image emerald_left_rest;
-	public static final transient Image emerald_left_1;
-	public static final transient Image emerald_left_2;
-	public static final transient Image emerald_right_rest;
-	public static final transient Image emerald_right_1;
-	public static final transient Image emerald_right_2;
-	public static final transient Image emerald_up_rest;
-	public static final transient Image emerald_up_1;
-	public static final transient Image emerald_up_2;
+	private static Image sprite_down_rest = new Image("file:images/emerald_down_rest.png"); 
+	private static Image sprite_down_1 = new Image("file:images/emerald_down_1.png");
+	private static Image sprite_down_2 = new Image("file:images/emerald_down_2.png");
+	private static Image sprite_left_rest = new Image("file:images/emerald_left_rest.png");
+	private static Image sprite_left_1 = new Image("file:images/emerald_left_1.png");
+	private static Image sprite_left_2 = new Image("file:images/emerald_left_2.png");
+	private static Image sprite_right_rest = new Image("file:images/emerald_right_rest.png");
+	private static Image sprite_right_1 = new Image("file:images/emerald_right_1.png");
+	private static Image sprite_right_2 = new Image("file:images/emerald_right_2.png");
+	private static Image sprite_up_rest = new Image("file:images/emerald_up_rest.png");;
+	private static Image sprite_up_1 = new Image("file:images/emerald_up_1.png");
+	private static Image sprite_up_2 = new Image("file:images/emerald_up_2.png");
 
-
-//	public static final Direction[] cachedValues = values();
 	
 	public static pictures getPictures(String key){
 		if(key.equals(KeyCode.LEFT.toString())){
