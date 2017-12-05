9b9aff41853e6628c21348521f4f4c3ea17221d7
cy122 <cy122@duke.edu>
2017-12-05 12:39:24 -0500

    change Direction

--- src/engine/movement/Direction.java
+++ src/engine/movement/Direction.java
@@ -9,41 +9,6 @@ import java.util.ArrayList;
  *
  */
 public class Direction {
-	
-	/**
-	 * the following commented code shouldn't be used anymore, for the case that different game has different moving images. ---- cy122
-	 */
-//	public enum pictures{
-//		UP(sprite_up_rest, sprite_up_1, sprite_up_2),
-//		DOWN(sprite_down_rest, sprite_down_1, sprite_down_2),
-//		LEFT(sprite_left_rest, sprite_left_1, sprite_left_2),
-//		RIGHT(sprite_right_rest, sprite_right_1, sprite_right_2);
-//		
-//		private ArrayList<Image> images = new ArrayList<Image>();
-//		
-//		pictures(Image image1, Image image2, Image image3) {
-//			images.add(image1);
-//			images.add(image2);
-//			images.add(image3);
-//		}
-//		
-//		public ArrayList<Image> getImages() {
-//			return images;
-//		}
-//	}
-//
-//	private static Image sprite_down_rest = new Image("file:images/emerald_down_rest.png"); 
-//	private static Image sprite_down_1 = new Image("file:images/emerald_down_1.png");
-//	private static Image sprite_down_2 = new Image("file:images/emerald_down_2.png");
-//	private static Image sprite_left_rest = new Image("file:images/emerald_left_rest.png");
-//	private static Image sprite_left_1 = new Image("file:images/emerald_left_1.png");
-//	private static Image sprite_left_2 = new Image("file:images/emerald_left_2.png");
-//	private static Image sprite_right_rest = new Image("file:images/emerald_right_rest.png");
-//	private static Image sprite_right_1 = new Image("file:images/emerald_right_1.png");
-//	private static Image sprite_right_2 = new Image("file:images/emerald_right_2.png");
-//	private static Image sprite_up_rest = new Image("file:images/emerald_up_rest.png");;
-//	private static Image sprite_up_1 = new Image("file:images/emerald_up_1.png");
-//	private static Image sprite_up_2 = new Image("file:images/emerald_up_2.png");
 
 	private final pictures up, down, left, right;
 	
