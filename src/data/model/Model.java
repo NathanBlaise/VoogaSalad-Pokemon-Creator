2c8b90038e3dcc2644db0d75e675be79a292f1fd
cy122 <cy122@duke.edu>
2017-12-06 23:00:38 -0500

    finish currency edit in author

--- src/data/model/Model.java
+++ src/data/model/Model.java
@@ -1,7 +1,15 @@
 package data.model;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.Serializable;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.List;
+
+import util.FilePathConverter;
+import data.items.Item;
 
 /**
  * Model holds the data which is unrelated with the actual movement/action of screen.
@@ -14,6 +22,42 @@ public class Model implements Serializable{
 	private ArrayList<NPC> NPCs= new ArrayList<NPC>(); // the NPC models
 	private ArrayList<PokemonSpecie> PokemonSpecies= new ArrayList<PokemonSpecie>(); // the pokemon models
 	private ArrayList<Tile> tiles = new ArrayList<Tile>();
+	private transient static final ArrayList<Item> items;
+	
+	static{
+		File folder = new File(FilePathConverter.getAbsolutePath("src/data/items"));
+		assert(folder.exists());
+		assert(folder.isDirectory());
+		File[] files = folder.listFiles(new FilenameFilter() {
+		    public boolean accept(File dir, String name) {
+		        return name.endsWith("java")&&(!name.equals("Item.java"));
+		    }
+		});
+		List<String> itemNames =  new ArrayList<String>();
+		for(File itemFile:files) {
+			String name = itemFile.getName();
+			int pos = name.lastIndexOf(".");
+			if (pos > 0) {
+			    name = name.substring(0, pos);
+			}
+			itemNames.add(name);
+		}
+		items = new ArrayList<Item>();
+		for(String name: itemNames){
+			try {
+				Class<?> itemClass = Class.forName("data.items."+name);
+				Constructor<?> itemConstructor = itemClass.getConstructor(new Class[]{});
+				Item tempItem = (Item) itemConstructor.newInstance();
+				items.add(tempItem);
+			} catch (ClassNotFoundException | NoSuchMethodException
+					| SecurityException | InstantiationException
+					| IllegalAccessException | IllegalArgumentException
+					| InvocationTargetException e) {
+				System.out.printf("wrong item class!");
+				e.printStackTrace();
+			}
+		}
+	}
 	
 	/**
 	 * WARNING!
--- src/data/player/Player.java
+++ src/data/player/Player.java
@@ -2,6 +2,7 @@ package data.player;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Map;
 
 import data.items.Item;
 import data.model.Pokemon;
@@ -9,16 +10,15 @@ import data.model.Pokemon;
 /**
  * The main character which the user plays the role of
  * 
- * @author 
+ * @author cy122 nathan
  *
  */
 public class Player implements Serializable{
 	private static final long serialVersionUID = 556462866183029469L;
 	private int posX, posY;
 	private Pokemon[] pokemons;
-	private ArrayList<Item> items;
 	private double currency;
-	//private Map<Item,Integer> Bag;  // a map from the item to the number of item
+	private Map<String, Integer> items;  // a map from the item to the number of item
 	private ArrayList<Pokemon> warehouse;
 	
 	public Player(){
@@ -49,16 +49,41 @@ public class Player implements Serializable{
 		return pokemons;
 	}
 	
-	public ArrayList<Item> getItems() {
+	public Map<String, Integer> getItems() {
 		return items;
 	}
 
-	public void setItems(ArrayList<Item> items) {
+	public void setItems(Map<String, Integer> items) {
 		this.items = items;
 	}
-	
+
 	public void addItem(Item item) {
-		items.add(item);
+		String itemName = item.getItemName();
+		if(items.containsKey(itemName)){
+			items.put(itemName, items.get(itemName)+1);
+		}else{
+			items.put(itemName, 1);
+		}
+	}
+	
+	/**
+	 * 
+	 * @param item - the goal item to be deleted
+	 * @return true if the item exists in the items and be deleted successfully.
+	 */
+	public boolean deleteItem(Item item){
+		String itemName = item.getItemName();
+		if(items.containsKey(itemName)){
+			if((items.get(itemName)-1)<0){
+				items.put(itemName, 0);
+				return false;
+			}else{
+				items.put(itemName, items.get(itemName)-1);
+				return true;
+			}
+		}else{
+			return false;
+		}
 	}
 
 	public void setPokemons(Pokemon[] pokemons) {
@@ -88,6 +113,13 @@ public class Player implements Serializable{
 		this.warehouse = warehouse;
 	}
 	
+	public int itemsSize(){
+		int number = 0;
+		for(String key: items.keySet()){
+			number += items.get(key);
+		}
+		return number;
+	}
 	
 	
 }
